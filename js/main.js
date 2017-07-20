5766df816aa2dadebd016d5b0bd3c9a0a0d16697
Laassari <nlaassari@gmail.com>
2017-07-20 04:30:27 +0000

    fixed responsiveness

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-(function(document){
+(function () {
 	var mainNav = document.getElementById('main-nav');
 	var toggleButton = document.getElementById('toggle-button');
-
-	toggleButton.addEventListener('click', function(){
+	
+	toggleButton.addEventListener('click', function(event){
 		mainNav.classList.toggle('hide-nav');
 	})
-})(document)
+	document.body.addEventListener('click', function(e){
+		if (e.target !== mainNav && e.target !== toggleButton) {
+			if (!mainNav.classList.contains('hide-nav')) {
+				mainNav.classList.toggle('hide-nav');
+			}
+		}
+
+	}, false)
+})()
